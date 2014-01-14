fa8dac14804baec056f8a7857d594b52d63319db
Robert Jacobs <info@robertjacobs.eu>
2014-01-14 12:31:49 +0100

    added missing files

---
+++ zuros_deliberator/build/CMakeFiles/movement.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/movement.dir/src/movement_main.cpp.o"
+  "CMakeFiles/movement.dir/src/movement.cpp.o"
+  "../bin/movement.pdb"
+  "../bin/movement"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/movement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
